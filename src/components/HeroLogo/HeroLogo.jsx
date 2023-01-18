c021420973e7d303700e07cfff85302c2b888f89
hevedrios <exxed.ridel@gmail.com>
2023-01-17 21:12:00 -0600

    ADD Hero Logo re-styled component

--- src/components/HeroLogo/HeroLogo.jsx
+++ src/components/HeroLogo/HeroLogo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { HeroLogoStyled } from "./HeroLogo.styles";
+import BgStudioX from "../../assets/studio-xperimental.jpg";
 import Logo from "../../assets/SoloX.svg";
 
 const HeroLogo = () => {
   return (
     <HeroLogoStyled>
-      <div className="logo">
-        <img src={Logo} alt="Logo Xperimental Sounds" />
+      <img src={BgStudioX} alt="Music Studio" />
+      <div className="overlaped">
+        <img className="logo" src={Logo} alt="Logo Xperimental Sounds" />
+        <p>Xperimental</p>
       </div>
     </HeroLogoStyled>
   );
--- src/components/HeroLogo/HeroLogo.styles.js
+++ src/components/HeroLogo/HeroLogo.styles.js
@@ -1,18 +1,32 @@
 import styled from "styled-components";
-import BgStudioX from "../../assets/studio-xperimental.jpg";
 
 export const HeroLogoStyled = styled.div`
-  min-height: 100vh;
-  background-repeat: no-repeat;
-  bacground-size: cover; // no effect
-  background-position: center center;
-  background-image: url(${BgStudioX});
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
 
-  .logo {
-    min-height: 100vh;
-    display: grid;
-    place-items: center;
-    padding-inline: 1rem;
-    padding-block: 2.5rem;
+  & > img {
+    background-size: cover;
+    width: 100%;
+    height: 725px;
+    object-fit: cover;
+  }
+
+  .overlaped {
+    display: block;
+    position: absolute;
+  }
+
+  .overlaped img {
+    width: calc(300px + 3vmin);
+    padding-inline: 2rem;
+  }
+
+  .overlaped p {
+   text-align: center;
+   font-size: calc(30px + 2vmin);
+   font-weight: bold;
   }
 `;
