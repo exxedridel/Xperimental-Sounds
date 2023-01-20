1e4bb5dedd1ccfe7fbd9b854cc102ef3da79a45a
hevedrios <exxed.ridel@gmail.com>
2023-01-20 01:43:11 -0600

    FIX Container class styles for better responsive design

--- src/components/Right-to-bottom/ContainerRtB.styles.js
+++ src/components/Right-to-bottom/ContainerRtB.styles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const ContainerRtBStyled = styled.div`
-   line-height: 1.6;
-   font-style: italic;
+  line-height: 1.6;
+  font-style: italic;
 
   & article {
     grid-area: article;
@@ -10,22 +10,21 @@ export const ContainerRtBStyled = styled.div`
 
   & img {
     grid-area: image;
-    height: 100%;
+    height: 550px;
+    width: 100%;
     object-fit: cover;
-  }
-
-  @media (max-width: 58em) {
-   & img {
-      margin-top: 2rem;
-   }
+    margin-top: 2rem;
   }
 
   @media (min-width: 58em) {
-   display: grid;
-   place-items: center;
+    display: grid;
+    place-items: center;
     column-gap: 2rem;
     grid-template-columns: repeat(2, minmax(0, 1fr));
-    grid-template-areas:
-      "article image";
+    grid-template-areas: "article image";
+    & img {
+      height: 100%;
+      margin-top: 0;
+    }
   }
 `;
