85f43d5207413f4daacedaa55a23e82fcdbb3d53
hevedrios <exxed.ridel@gmail.com>
2023-01-20 18:08:30 -0600

    ADD AccordionBS component and bootstrap v5.3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/global.css";
 import HeroLogo from "./components/Hero-Logo/HeroLogo";
 import GridContainer from "./components/Grid-container/GridContainer";
 import FlexCards from "./components/Flex-cards/FlexCards";
+import AccordionBS from "./components/Accordion-BS/AccordionBS";
 
 const App = () => {
   return (
@@ -15,13 +16,25 @@ const App = () => {
           incubadora musical
         </h1>
         <div style={{ borderBottom: "1px solid white", width: "100px" }}></div>
-        <p className="text-accent">León, Guanajuato, México</p>
+        <p className="uppercase text-accent ff-sans-cond letter-spacing-3">
+          León, Guanajuato, México
+        </p>
       </div>
       <div className="container">
         <GridContainer />
       </div>
       <div>
         <FlexCards />
+      </div>
+      <div className="container flow">
+        <div className="flex-center text-centered">
+          <h2 className="uppercase ff-sans-cond letter-spacing-3 text-centered">
+            Preguntas frecuentes
+          </h2>
+          <div style={{ borderBottom: "1px solid white", width: "100px" }}></div>
+        </div>
+
+        <AccordionBS />
       </div>
       <div className="footer"></div>
     </div>
