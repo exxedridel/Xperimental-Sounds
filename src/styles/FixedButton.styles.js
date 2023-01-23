d5c73c9442f19e6f64950f23c64c7891c7ec3231
hevedrios <exxed.ridel@gmail.com>
2023-01-23 13:24:18 -0600

    FIX Fixed button padding

--- src/styles/FixedButton.styles.js
+++ src/styles/FixedButton.styles.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const FixedButton = styled.img`
   position: fixed;
   z-index: 1000;
-  bottom:54px;
-  right: clamp(0.1em, 5vw, 8em);
+  bottom: clamp(0.1em, 4vw, 8em);
+  right: clamp(0.1em, 4vw, 8em);
 
   @media (min-width: 65em) {
     & {
