419f49c256957ac9d6b23abd7a39e53ebcddc7cc
hevedrios <exxed.ridel@gmail.com>
2023-01-23 15:27:17 -0600

    ADD Webpack setup to handle favicon

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   mode: "development",
   resolve: {
@@ -44,6 +45,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "./index.html",
+      favicon: "./src/assets/favicon.gif",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
